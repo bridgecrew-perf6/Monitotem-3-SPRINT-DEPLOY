63735d3198d25b5ca9df5ba9387c7ef7e36eae6d
Silvestre, Enzo Fabrizio Brigante <enzo.f.silvestre@accenture.com>
2022-05-22 18:42:03 -0300

    att dash

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -47,16 +47,21 @@ function listarTotem() {
     const totens = await resposta.json();
     for (let i = 0; i < totens.length; i++) {
       let content = document.createElement("div");
-      let content_id = document.createElement('span')
+      let content_id = document.createElement('div')
       let content_status = document.createElement('span')
       let content_action = document.createElement('span')
 
-      content_id.innerHTML = `${totens[i].idTotem}`;
+      content_id.innerHTML = `<i class="fa-solid fa-display"></i>${totens[i].idTotem}`;
       content_status.innerHTML = 'Instável'
-      content_action.innerHTML = 'teste'
+      content_action.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i>'
 
 
       content.setAttribute("class", "content")
+      content_id.setAttribute("class", "content_id")
+      content_status.setAttribute("class", "content_status")
+      content_action.setAttribute("class", "content_action")
+
+
 
       content.append(content_id, content_status, content_action);
 
@@ -66,9 +71,3 @@ function listarTotem() {
 }
 
 window.addEventListener("load", listarTotem);
-
-
-
-
-
-
